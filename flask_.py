8eb844c186ced27dd5d51e3539ff2270748f1b6c
Kirill <xfiendx4life@gmail.com>
2017-02-18 15:33:28 +0300

    Added get_hash function to encrypt password and started login handler

--- flask_.py
+++ flask_.py
@@ -21,7 +21,14 @@ def render_str(template,**params):
 def render(template, **kw):
    return render_str(template, **kw)
 
+def get_hash(password):
+   return hashlib.md5(password.encode('utf-8')).hexdigest()
 
+def valid_login(login,password):
+   people = db_query()
+   for person in people:
+      if person.name == login:
+         
 
 @app.route('/')
 def hello_world():
@@ -46,7 +53,7 @@ def welcome():
                        password_visibility="visible",
                                      password = "You've already registered"))
          resp.set_cookie('username', login)
-         resp.set_cookie('password', hashlib.md5(password.encode('utf-8')).hexdigest())
+         resp.set_cookie('password', get_hash(password))
          return resp
    return render('index.html', text=' - Welcome, Stranger. What is your name? ',
                  password_visibility="hidden")
@@ -56,6 +63,18 @@ def table():
    people = db_query()
    if request.method == "GET":
       return render('table.html', base = people)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
+   
    
 
 if __name__ == '__main__':
